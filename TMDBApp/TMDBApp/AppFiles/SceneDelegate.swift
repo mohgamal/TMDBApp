06e89c02cd938c169aeeef6bc60c9def0766939e
mohabdelaty <mabdelaty@worldremit.com>
2020-10-27 08:08:53 +0200

    Update scene delegate with required launch view

--- TMDBApp/TMDBApp/AppFiles/SceneDelegate.swift
+++ TMDBApp/TMDBApp/AppFiles/SceneDelegate.swift
@@ -5,7 +5,8 @@
 //  Created by Mohammed Gamal on 10/27/20.
 //
 
-import UIKit
+import SwiftUI
+import TMDBPresentation
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -13,10 +14,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+  
+        let discoverView = DiscoverView(appDI: AppDI.shared,
+                                        discoverVM: AppDI.shared.discoverDependencies())
+        
+        if let windowScene = scene as? UIWindowScene {
+                   let window = UIWindow(windowScene: windowScene)
+                   window.rootViewController = UIHostingController(rootView: discoverView)
+                   self.window = window
+                   window.makeKeyAndVisible()
+               }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
