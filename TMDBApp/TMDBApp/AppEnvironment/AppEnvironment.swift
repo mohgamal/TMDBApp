2928798a4692f2db5d30ade084be1845827da671
mohabdelaty <mabdelaty@worldremit.com>
2020-10-27 08:13:15 +0200

    update iOS target development

--- TMDBApp/TMDBApp/AppEnvironment/AppEnvironment.swift
+++ TMDBApp/TMDBApp/AppEnvironment/AppEnvironment.swift
@@ -11,5 +11,5 @@ class AppEnvironment {
     
     static let baseURL = "https://api.themoviedb.org/3/"
     
-    let discoverMovies = "\(baseURL)discover/movie?sort_by=popularity.desc&api_key=\(Utils.apiKey)"
+    let discoverMovies = "\(baseURL)discover/movie?sort_by=popularity.desc&api_key=3e2339260d2b4cb99301e76c562061f5"
 }
